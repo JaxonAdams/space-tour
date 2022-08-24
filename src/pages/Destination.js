35d64bdc6b80f8c0d922204ecb2df588fb83ff1d
Jaxon Adams <jadamsresume1@gmail.com>
2022-08-24 17:08:45 -0600

    main page styles and dest navbar

--- src/pages/Destination.js
+++ src/pages/Destination.js
@@ -1,11 +1,33 @@
 import React from 'react';
 
+import moon from '../assets/destination/image-moon.png';
+
 import Header from '../components/Header';
 
 const Destination = () => {
     return (
         <div className='destination'>
             <Header />
+            <h1 className='destination-txt-lg'><span>01</span> PICK YOUR DESTINATION</h1>
+            <div className='destination-container'>
+                <img className='destination-img' src={moon} alt='your destination' />
+                <div className='dest-info-container'>
+                    <ul className='dest-nav'>
+                        <li className='dest-link'>
+                            MOON
+                        </li>
+                        <li className='dest-link'>
+                            MARS
+                        </li>
+                        <li className='dest-link'>
+                            EUROPA
+                        </li>
+                        <li className='dest-link'>
+                            TITAN
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,8 @@ import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 
 const Home = () => {
+    console.log(window.innerWidth, window.innerHeight);
+
     return (
         <div className='homepage'>
             <Header />
